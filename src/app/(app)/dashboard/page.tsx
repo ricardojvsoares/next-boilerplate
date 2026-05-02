1b778c5f1c58e80db00c14881d15d52174e29168
Ricardo Soares <soaresricaventura@gmail.com>
2026-05-02 15:07:41 +0100

    refactor: remove BreadcrumbDashboard component and integrate Breadcrumb directly in DashboardPage

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,31 +1,45 @@
-import { Page } from '@/components/pages-layout';
+import {
+  Page,
+  PageHeader,
+  PageIcon,
+  PageTitle,
+  PageActions,
+  PageContent,
+} from '@/components/shared/pages-layout';
 import { Button } from '@/components/ui/button';
 import { Home } from 'lucide-react';
-import { BreadcrumbDashboard } from './_components/dashboard.breadcrumbs';
 import { useTranslations } from 'next-intl';
+import { BreadCrumb, type BreadCrumbItems } from '@/components/shared/breadcrumbs';
+
+const navigationData: BreadCrumbItems = [
+  {
+    value: 'dashboard',
+    href: '/dashboard',
+  },
+];
 
 export default function DashboardPage() {
   const t = useTranslations('Dashboard');
   return (
     <Page>
-      <BreadcrumbDashboard />
-      <Page.Header>
+      <BreadCrumb items={navigationData} />
+      <PageHeader>
         <div className="display:block flex items-center gap-2">
-          <Page.Icon icon={Home} />
-          <Page.Title>{t('title')}</Page.Title>
+          <PageIcon icon={Home} />
+          <PageTitle>{t('title')}</PageTitle>
         </div>
-        <Page.Actions>
+        <PageActions>
           <Button>{t('new')}</Button>
-        </Page.Actions>
-      </Page.Header>
+        </PageActions>
+      </PageHeader>
 
-      <Page.Content>
+      <PageContent>
         <div className="border-muted rounded-lg border-2 border-dashed p-6 text-center">
           <Home className="mx-auto mb-4 size-10" />
           <h2 className="text-lg font-semibold">{t('welcome')}</h2>
           <p className="text-muted-foreground text-sm">{t('welcomeMessage')}</p>
         </div>
-      </Page.Content>
+      </PageContent>
     </Page>
   );
 }
