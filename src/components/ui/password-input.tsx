4901ac9075237270393f9e81b287a83bc65d4a9d
Ricardo Soares <soaresricaventura@gmail.com>
2026-05-03 11:52:47 +0100

    feat: refactor PasswordInput component for improved toggle functionality and styling

--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -1,45 +1,36 @@
 import { useState } from 'react';
-
-import { Eye, EyeOff } from 'lucide-react';
-
+import { EyeIcon, EyeOffIcon } from 'lucide-react';
 import { Input } from '@/components/ui/input';
-
 import { Button } from '@/components/ui/button';
-
 import { forwardRef } from 'react';
 
 interface PasswordInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   showToggle?: boolean;
 }
-
 export const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
-  ({ showToggle = true, className, ...props }, ref) => {
+  ({ ...props }, ref) => {
     const [showPassword, setShowPassword] = useState(false);
 
     return (
       <div className="relative">
         <Input
+          id={props.id}
           ref={ref}
-          type={showPassword ? 'text' : 'password'}
-          className={className}
           {...props}
+          type={showPassword ? 'text' : 'password'}
+          placeholder="Password"
+          className="pr-9"
         />
-
-        {showToggle && (
-          <Button
-            type="button"
-            variant="ghost"
-            size="sm"
-            className="absolute top-1/2 right-2 h-6 w-6 -translate-y-1/2 transform p-0"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? (
-              <Eye className="text-muted-foreground h-4 w-4" />
-            ) : (
-              <EyeOff className="text-muted-foreground h-4 w-4" />
-            )}
-          </Button>
-        )}
+        <Button
+          type="button"
+          variant="ghost"
+          size="icon"
+          onClick={() => setShowPassword((prevState) => !prevState)}
+          className="text-muted-foreground focus-visible:ring-ring/50 absolute inset-y-0 right-0 rounded-l-none hover:bg-transparent"
+        >
+          {showPassword ? <EyeOffIcon /> : <EyeIcon />}
+          <span className="sr-only">{showPassword ? 'Hide password' : 'Show password'}</span>
+        </Button>
       </div>
     );
   }
